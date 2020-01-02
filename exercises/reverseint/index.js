29acfa5d57f98e613cacba33ad5ec308d4e77d9a
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2020-01-02 16:17:14 -0500

    MY SOLUTION - REVERSEINT

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,38 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// ========================================================================================================================
+// MY SOLUTION (20mins)
+function reverseInt(n) {
+  // Convert int to str
+  let revInt;
+
+  // Handle Negative
+  if (n < 0) {
+    revInt = String(n * -1);
+    revInt =
+      Number(
+        revInt
+          .split("")
+          .reverse()
+          .join("")
+      ) * -1;
+    //   revInt = revInt * -1;
+    return revInt;
+    // Handle Zero
+  } else if (n == 0) {
+    return 0;
+    // Handle Positive Number
+  } else {
+    revInt = String(n);
+    revInt = Number(
+      revInt
+        .split("")
+        .reverse()
+        .join("")
+    );
+    return revInt;
+  }
+}
 
 module.exports = reverseInt;
