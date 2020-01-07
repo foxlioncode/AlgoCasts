b773d4d3ee654525c9b46f3ad8d08f187f620ce9
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2020-01-06 20:51:09 -0500

    SOLUTION #1 - KEEP IT SIMPLE

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,24 +13,50 @@
 //   buzz
 
 // ========================================================================================================================
+// SOLUTION #1
+
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+
+    // Check if i is a multiple of 3 and 5.
+    // if (i % 15 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+
+      // Check if i is a multiple of 3.
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+
+      // Check if i is a multiple of 5.
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+      
+    } else {
+      // Print i
+      console.log(i);
+    }
+  }
+}
+
+// ========================================================================================================================
 // ## MY SOLUTION
 // - Remainder
 // - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder>
 
-function fizzBuzz(n) {
+// function fizzBuzz(n) {
 
-for (let i = 1; i <= n; i++)
-    
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('fizzbuzz');
-    } else if (i % 3 === 0) {
-        console.log('fizz');
-    } else if (i % 5 === 0) {
-        console.log('buzz');
-    } else {
-        console.log(i);
-    }
+// for (let i = 1; i <= n; i++)
 
-}
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('fizzbuzz');
+//     } else if (i % 3 === 0) {
+//         console.log('fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('buzz');
+//     } else {
+//         console.log(i);
+//     }
+
+// }
 
 module.exports = fizzBuzz;
