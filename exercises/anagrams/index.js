64216352f1a11b6a466b43d79dda563d535e218e
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2020-01-10 14:02:05 -0500

    SOLUTION #1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,85 +9,116 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 // ========================================================================================================================
-// MY SOLUTION
-// - RegExp
-// - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions>
-// - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp>
-// - String.prototype.replace()
-// - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace>
-// - String.prototype.toLowerCase()
-// - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase>
+// SOLUTION #1
 
 function anagrams(stringA, stringB) {
-  let mapObjectA = mapObjectArrFunction(stringA, stringB)[0];
-  let mapObjectB = mapObjectArrFunction(stringA, stringB)[1];
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
 
-  console.log("mapObjectA", mapObjectA);
-  console.log("mapObjectB", mapObjectB);
-  let mapObjectAKeys = Object.keys(mapObjectA);
-  let mapObjectBKeys = Object.keys(mapObjectB);
-  let mapObjectAValues = Object.values(mapObjectA);
-  let mapObjectBValues = Object.values(mapObjectB);
-
-  if (mapObjectAKeys.length != mapObjectBKeys.length) {
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
     return false;
-  } else {
-    let returnValue;
-    for (let element of mapObjectAKeys) {
-      if (!mapObjectBKeys.includes(element)) {
-        returnValue = false;
-      } else {
-        returnValue = true;
-      }
-    }
-    return returnValue;
   }
-}
 
-function mapObjectArrFunction(stringA, stringB) {
-  let cleanStrArr = cleanStrArrFunction(stringA, stringB);
-  //   ------------------------------------------------------------------------------------------------------------------------
-  // Create comparison object from clean strings.
-  let mapObjectArr = [];
-  for (let cleanStr of cleanStrArr) {
-    mapObjectArr.push(mapString(cleanStr));
-  }
-  return mapObjectArr;
-}
-
-//   ------------------------------------------------------------------------------------------------------------------------
-// Clean up the two strings.
-function cleanStrArrFunction(stringA, stringB) {
-  let strArr = [stringA, stringB];
-  let cleanStrArr = [];
-  let cleanStrA = stringA;
-  let cleanStrB = "";
-  // Regex Syntax `/pattern/flags`
-  const regexWhiteSpace = /\s/g;
-  const regexSpecialCharacters = /\W/g;
-  for (let strArrayElement of strArr) {
-    strArrayElement = strArrayElement.replace(regexWhiteSpace, "");
-    strArrayElement = strArrayElement.replace(regexSpecialCharacters, "");
-    strArrayElement = strArrayElement.toLowerCase();
-    cleanStrArr.push(strArrayElement);
-  }
-  // Return cleanStrArr
-  return cleanStrArr;
-}
-
-//   ------------------------------------------------------------------------------------------------------------------------
-// Map each cleaned string.
-function mapString(str) {
-  let strMapObject = {};
-  for (let char of str) {
-    if (!strMapObject[char]) {
-      strMapObject[char] = 1;
-    } else {
-      strMapObject[char]++;
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
     }
   }
-  return strMapObject;
+  
+  return true;
 }
+
+// Helper Function - Build a character map for each string.
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
+
+// ========================================================================================================================
+// // MY SOLUTION
+// // - RegExp
+// // - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions>
+// // - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp>
+// // - String.prototype.replace()
+// // - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace>
+// // - String.prototype.toLowerCase()
+// // - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase>
+
+// function anagrams(stringA, stringB) {
+//   let mapObjectA = mapObjectArrFunction(stringA, stringB)[0];
+//   let mapObjectB = mapObjectArrFunction(stringA, stringB)[1];
+
+//   console.log("mapObjectA", mapObjectA);
+//   console.log("mapObjectB", mapObjectB);
+//   let mapObjectAKeys = Object.keys(mapObjectA);
+//   let mapObjectBKeys = Object.keys(mapObjectB);
+//   let mapObjectAValues = Object.values(mapObjectA);
+//   let mapObjectBValues = Object.values(mapObjectB);
+
+//   if (mapObjectAKeys.length != mapObjectBKeys.length) {
+//     return false;
+//   } else {
+//     let returnValue;
+//     for (let element of mapObjectAKeys) {
+//       if (!mapObjectBKeys.includes(element)) {
+//         returnValue = false;
+//       } else {
+//         returnValue = true;
+//       }
+//     }
+//     return returnValue;
+//   }
+// }
+
+// function mapObjectArrFunction(stringA, stringB) {
+//   let cleanStrArr = cleanStrArrFunction(stringA, stringB);
+//   //   ------------------------------------------------------------------------------------------------------------------------
+//   // Create comparison object from clean strings.
+//   let mapObjectArr = [];
+//   for (let cleanStr of cleanStrArr) {
+//     mapObjectArr.push(mapString(cleanStr));
+//   }
+//   return mapObjectArr;
+// }
+
+// //   ------------------------------------------------------------------------------------------------------------------------
+// // Clean up the two strings.
+// function cleanStrArrFunction(stringA, stringB) {
+//   let strArr = [stringA, stringB];
+//   let cleanStrArr = [];
+//   let cleanStrA = stringA;
+//   let cleanStrB = "";
+//   // Regex Syntax `/pattern/flags`
+//   const regexWhiteSpace = /\s/g;
+//   const regexSpecialCharacters = /\W/g;
+//   for (let strArrayElement of strArr) {
+//     strArrayElement = strArrayElement.replace(regexWhiteSpace, "");
+//     strArrayElement = strArrayElement.replace(regexSpecialCharacters, "");
+//     strArrayElement = strArrayElement.toLowerCase();
+//     cleanStrArr.push(strArrayElement);
+//   }
+//   // Return cleanStrArr
+//   return cleanStrArr;
+// }
+
+// //   ------------------------------------------------------------------------------------------------------------------------
+// // Map each cleaned string.
+// function mapString(str) {
+//   let strMapObject = {};
+//   for (let char of str) {
+//     if (!strMapObject[char]) {
+//       strMapObject[char] = 1;
+//     } else {
+//       strMapObject[char]++;
+//     }
+//   }
+//   return strMapObject;
+// }
 
 // ========================================================================================================================
 module.exports = anagrams;
