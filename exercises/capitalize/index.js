a4069074eb3835f27b91b34c13c39faa34918136
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2020-01-10 21:35:33 -0500

    SOLUTION #2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,19 +8,34 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 // ========================================================================================================================
+// SOLUTION #2
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+// ========================================================================================================================
 // SOLUTION #1
 // Array.prototype.slice()f
 // - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice>
 // String.prototype.toUpperCase()
 // - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase>
 
-function capitalize(str) {
-    const words = [];
-    for (let word of str.split(' ')) {
-        words.push(word[0].toUpperCase() + word.slice(1));
-    }
-    return words.join(' ');
-}
+// function capitalize(str) {
+//     const words = [];
+//     for (let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     return words.join(' ');
+// }
 
 // ========================================================================================================================
 // MY SOLUTION
