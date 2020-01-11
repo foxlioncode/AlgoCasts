27c1eb896bc66864421c46603cdd8bab071b8032
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2020-01-10 20:49:32 -0500

    SOLUTION #1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,21 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
+// ========================================================================================================================
+// SOLUTION #1
+// Array.prototype.slice()f
+// - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice>
+// String.prototype.toUpperCase()
+// - <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase>
+
 function capitalize(str) {
-  let strArr = str.split(" ");
-  let capStrArr = [];
-
-  for (let element of strArr) {
-    capStrArr.push(capFirstLetterStr(element));
-  }
-
-  return capStrArr.join(" ");
+    const words = [];
+    for (let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+    return words.join(' ');
 }
 
-function capFirstLetterStr(str) {
-  let firstLetterCapStr = str.split("");
-  firstLetterCapStr[0] = firstLetterCapStr[0].toUpperCase();
-  return firstLetterCapStr.join("");
-}
+// ========================================================================================================================
+// MY SOLUTION
+// function capitalize(str) {
+//   let strArr = str.split(" ");
+//   let capStrArr = [];
+
+//   for (let element of strArr) {
+//     capStrArr.push(capFirstLetterStr(element));
+//   }
+
+//   return capStrArr.join(" ");
+// }
+
+// function capFirstLetterStr(str) {
+//   let firstLetterCapStr = str.split("");
+//   firstLetterCapStr[0] = firstLetterCapStr[0].toUpperCase();
+//   return firstLetterCapStr.join("");
+// }
 
 module.exports = capitalize;
