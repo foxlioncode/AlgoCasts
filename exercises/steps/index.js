325d12674c3cbb86c13492a182eb9192081b63fa
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2020-01-12 21:04:47 -0500

    SOLUTION #1 - Iterative

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -18,42 +18,61 @@
 //       '####'
 
 // ========================================================================================================================
-// MY SOLUTION
+// SOLUTION #1 - Iterative/Matrix Solution
+// - Rows
+// - Columns
 
 function steps(n) {
-  let countPounds = 0;
-  let countSpaces = n;
-  for (let i = 0; i <= n; i++) {
-    if (i === 0) {
-      countPounds++;
-      countSpaces--;
-    } else {
-      console.log(addSpaces(addPounds(countPounds), countSpaces));
-      countPounds++;
-      countSpaces--;
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
     }
+    console.log(stair);
   }
 }
 
-function addPounds(numberOfPounds) {
-  let poundsOutput = "";
-  let count = 0;
-  while (count < numberOfPounds) {
-    poundsOutput += "#";
-    count++;
-  }
-  return poundsOutput;
-}
+// ========================================================================================================================
+// MY SOLUTION
 
-function addSpaces(inputString, numberOfSpaces) {
-  let spacesOutput = inputString;
-  let count = 0;
-  while (count < numberOfSpaces) {
-    spacesOutput += " ";
-    count++;
-  }
-  return spacesOutput;
-}
+// function steps(n) {
+//   let countPounds = 0;
+//   let countSpaces = n;
+//   for (let i = 0; i <= n; i++) {
+//     if (i === 0) {
+//       countPounds++;
+//       countSpaces--;
+//     } else {
+//       console.log(addSpaces(addPounds(countPounds), countSpaces));
+//       countPounds++;
+//       countSpaces--;
+//     }
+//   }
+// }
+
+// function addPounds(numberOfPounds) {
+//   let poundsOutput = "";
+//   let count = 0;
+//   while (count < numberOfPounds) {
+//     poundsOutput += "#";
+//     count++;
+//   }
+//   return poundsOutput;
+// }
+
+// function addSpaces(inputString, numberOfSpaces) {
+//   let spacesOutput = inputString;
+//   let count = 0;
+//   while (count < numberOfSpaces) {
+//     spacesOutput += " ";
+//     count++;
+//   }
+//   return spacesOutput;
+// }
 
 // steps(5);
 
