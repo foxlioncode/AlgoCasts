8c2057bbd1738f573f4323857ce06b51e499db45
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2020-01-12 21:55:41 -0500

    SOLUTION #2 - Recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -18,23 +18,63 @@
 //       '####'
 
 // ========================================================================================================================
+// SOLUTION #2 - Recursion
+// - Recursion Tips
+//     - Figure out the bare minimum pieces of infomraiton to represent your problem.
+//     - Give reasonable defaults to the bare minimum pieces of information.
+//     - Check the base case. Is there any work left to do? If not, return.
+//     - Do some work. Call your function again, making sure the arguments have changed in some fashion.
+
+// function printNumber(n, dec = 1) {
+// // Identify the base case, where there is no more work to do and to return the function. Nailing your base case is very important. Do it at the beginning.
+//   if (n === 0) {
+//     return;
+//   }
+//   console.log(n);
+// // This is the recursive step.
+//   printNumber(n - dec);
+// }
+// printNumber(10);
+
+function steps(n, row = 0, stair = "") {
+  // Check the base case.
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  steps(n, row, stair);
+
+}
+
+// ========================================================================================================================
 // SOLUTION #1 - Iterative/Matrix Solution
 // - Rows
 // - Columns
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = "";
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += "#";
-      } else {
-        stair += " ";
-      }
-    }
-    console.log(stair);
-  }
-}
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
 
 // ========================================================================================================================
 // MY SOLUTION
