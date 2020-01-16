8fb48d9723865841302504fd08da8529e22d0104
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2020-01-15 23:25:51 -0500

    MY SOLUTION - MATRIX

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,49 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// ========================================================================================================================
+// MY SOLUTION
+function matrix(n) {
+  // Create Matrix n x n
 
+  let matrixStructure = [];
+  let rowArr = [];
+  let iterateCount = n * n;
+  let displayIterate = 1;
+
+  // Push element into rowArr
+  for (let i = 0; i < iterateCount; i++) {
+    if (rowArr.length == n) {
+      matrixStructure.push(rowArr);
+      rowArr = [];
+      rowArr.push(displayIterate);
+      displayIterate++;
+    } else {
+      rowArr.push(displayIterate);
+      displayIterate++;
+    }
+  }
+
+  matrixStructure.push(rowArr);
+  rowArr = [];
+
+  replaceMatrixElement(matrixStructure, 0, 1, "hello");
+
+  // Spiral Algorithm
+  // Replace going right to the end.
+  // Replace going down to the end.
+  // Replace going left to the end.
+  // Replace going up to the end.
+  // Reduce count
+
+  return matrixStructure;
+}
+
+function replaceMatrixElement(matrix, row, column, newElement) {
+  matrix[row][column] = newElement;
+}
+
+console.log(matrix(4));
+
+// ========================================================================================================================
 module.exports = matrix;
